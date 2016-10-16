ad98ea6e6a7126800e899c03d22267efc7066c49
laurenkl96 <lklynch@ksu.edu>
2016-10-16 15:49:48 -0500

    still working

--- src/app.js
+++ src/app.js
@@ -3,12 +3,23 @@
 /* Classes */
 const Game = require('./game.js');
 const Player = require('./player.js');
+const Asteroid = require('./asteroid.js');
+cons Vector = require('./vector.js');
 
 /* Global variables */
 var canvas = document.getElementById('screen');
 var game = new Game(canvas, update, render);
 var player = new Player({x: canvas.width/2, y: canvas.height/2}, canvas);
-
+var asteroids = [];
+var axisList = [];
+for(var a = 0; a < 20; a++){
+  asteroids.push(
+    new Asteroid({x: Math.random() * canvas.width, y: Math.random()* canvas.height}, canvas);
+  )
+  axisList.push(asteroids[a]);
+}
+axisList.push(player);
+axisList.sort(function(a,b){return a.position.x - b.position.x})
 /**
  * @function masterLoop
  * Advances the game in sync with the refresh rate of the screen
@@ -31,6 +42,17 @@ masterLoop(performance.now());
  */
 function update(elapsedTime) {
   player.update(elapsedTime);
+  asteroids.forEach(function (a){
+    a.update(elapsedTime);
+  });
+  axisList.sort(function(a,b){return a.position.x - b.position.x});
+  var active = [];
+  var potentials = [];
+  axisList.forEach(function(obj, index){
+    active = active.filter(function(act){
+      return obj.position.x - act.position.x
+    })
+  })
   // TODO: Update the game objects
 }
 
@@ -45,4 +67,7 @@ function render(elapsedTime, ctx) {
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   player.render(elapsedTime, ctx);
+  asteroids.forEach(function(a){
+    a.render(elapsedTime, ctx);
+  })
 }
